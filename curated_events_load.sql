6c746ca8d852abe601e8129cfa5d9cdf21044ce6
subash76285 <97172681+subash76285@users.noreply.github.com>
2026-01-06 11:25:22 +0530

    Revise curated_events_load.sql for new schema and logic
    Updated the schema and table creation for curated events, including new fields and data transformations.

--- curated_events_load.sql
+++ curated_events_load.sql
@@ -1,9 +1,13 @@
 create schema if not exists curatedds;
 
-create or replace table if not exists curatedds.curated_events(event_id INT64, user_id INT64, event_name string, event_timestamp timestamp, event_date date, source string, channel string);
 
-insert into curatedds.curated_events select user_id, event_id, event_name, event_ts, source, channel, row_number() over (partition by user_id order by event_ts) as page_order from (SELECT event_id, user_id, event_name, cast(event_timestamp as timestamp) as event_ts, cast(event_date as date) as event_date, source, channel from rawds.raw_events where user_id is not NULL and event_id is not NULL and channel is not NULL);
+create or replace table curatedds.curated_events as select user_pseudo_id, event_name, TIMESTAMP(event_timestamp) AS event_time, 
+  COALESCE((select value.string_value from unnest(event_params) where key = 'source'), traffic_source.source, 'direct') AS source,
+  COALESCE((select value.string_value from unnest(event_params) where key = 'medium'), traffic_source.medium, '(none)') AS medium,
+  (select value.int_value from unnest(event_params) where key = 'value') as event_value from rawds.raw_events;
 
-select * from `curatedds.curated_events` limit 5;
+create or replace table curatedds.curated_events_final as select user_pseudo_id, event_name, event_time, source, medium, event_value, ROW_NUMBER() OVER (PARTITION by user_pseudo_id order by event_time) AS event_order from stg_ga4_events;
+
+select * from `curatedds.curated_events_final` limit 5;
 
 select current_timestamp,"Curated Load completed Successfully";
