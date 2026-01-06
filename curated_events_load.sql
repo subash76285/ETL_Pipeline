6054c57e8936940efcae0adac786be9b8a04d44a
subash76285 <97172681+subash76285@users.noreply.github.com>
2026-01-06 11:33:53 +0530

    Refactor curated_events SQL loading process

--- curated_events_load.sql
+++ curated_events_load.sql
@@ -6,8 +6,9 @@ create or replace table curatedds.curated_events as select user_pseudo_id, event
   COALESCE((select value.string_value from unnest(event_params) where key = 'medium'), traffic_source.medium, '(none)') AS medium,
   (select value.int_value from unnest(event_params) where key = 'value') as event_value from rawds.raw_events;
 
-create or replace table curatedds.curated_events_final as select user_pseudo_id, event_name, event_time, source, medium, event_value, ROW_NUMBER() OVER (PARTITION by user_pseudo_id order by event_time) AS event_order from stg_ga4_events;
+create or replace table curatedds.curated_events_final as select user_pseudo_id, event_name, event_time, source, medium, event_value, ROW_NUMBER() OVER (PARTITION by user_pseudo_id order by event_time) AS event_order from curatedds.curated_events;
 
 select * from `curatedds.curated_events_final` limit 5;
 
 select current_timestamp,"Curated Load completed Successfully";
+
