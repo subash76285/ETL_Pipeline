5e436e00b7341ed0901843c04b18d3b5c99ba9b7
subash76285 <97172681+subash76285@users.noreply.github.com>
2026-01-06 11:08:35 +0530

    Change data load format to JSON for raw_events
    Updated the data loading format from CSV to JSON and modified the source file.

--- raw_events_load.sql
+++ raw_events_load.sql
@@ -1,9 +1,7 @@
 
 create schema if not exists rawds;
 
-create table if not exists rawds.raw_events(event_id INT64, user_id INT64, event_name string, event_timestamp string, event_date string, source string, channel string);
-
-LOAD DATA OVERWRITE `rawds.raw_events` FROM FILES (format = 'CSV', uris = ['gs://analytics-bucket/data/events.csv'], field_delimiter=',');
+LOAD DATA OVERWRITE `rawds.raw_events` FROM FILES (format = 'JSON', uris = ['gs://analytics-bucket/data/raw_ga4_events.json']);
 
 select * from `rawds.raw_events` limit 5;
 
